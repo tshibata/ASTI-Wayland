6b20e087237d78cee44264e5efed8f7a0df67d5c
tshibata <tshibata@boutem.com>
2017-07-04 22:08:37 +0200

    xkbcommon requires bison

--- xkbcommon.sh
+++ xkbcommon.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# xkbcommon
+#   xkbcommon - XKB API common to servers and clients
+
 set -e
+
+# It requires:
+bison --version || ./bison.sh
 
 wget --no-clobber https://xkbcommon.org/download/libxkbcommon-0.7.1.tar.xz
 
