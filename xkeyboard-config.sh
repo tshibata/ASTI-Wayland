01b2897ea939bdf6926c5db6b3bbb0b5451ce92a
tshibata <tshibata@boutem.com>
2017-07-05 23:06:30 +0200

    xkeyboard-config requires intltool

--- xkeyboard-config.sh
+++ xkeyboard-config.sh
@@ -6,6 +6,7 @@
 set -e
 
 # It requires:
+pkg-config --exists 'intltool >= 0.30' || ./intltool.sh
 perl -e "require XML::Parser;" || ./Perl-XML-Parser.sh
 
 wget --no-clobber https://www.x.org/archive/individual/data/xkeyboard-config/xkeyboard-config-2.20.tar.bz2
